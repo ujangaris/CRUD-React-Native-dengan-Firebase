f9ed50d8181e06fbee1fed22b8125ec3cac80f39
ujangaja <ujangaja@gmail.com>
2021-09-19 14:19:03 +0700

    Tutorial React Native Dasar : #6. Belajar State dan Props | memanggil State cara 1

--- src/StateProps.js
+++ src/StateProps.js
@@ -2,13 +2,24 @@ import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 export default class StateProps extends Component {
+  //rcons
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sekolah: 'Wahidev Academy',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}> Belajar State dan Props</Text>
         <View style={styles.garis} />
         {/* Isi */}
-        <View style={styles.isi}></View>
+        <View style={styles.isi}>
+          <Text>Ini adalah State : {this.state.sekolah}</Text>
+        </View>
       </View>
     );
   }
